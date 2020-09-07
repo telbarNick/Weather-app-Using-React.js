b1990b4e5083c5f5a6b8d8c6cbba66aec5eb19eb
Dronov Nickolay <dronovnick@gmail.com>
2020-09-07 19:02:29 +0300

    Updates

---
+++ precache-manifest.a81b009a0a3f9bf36ed7f64978f92f77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b27000290b6a79e156fb2641ecd9ef",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8105a43483487808da49",
+    "url": "/weather/static/css/main.e9e060db.chunk.css"
+  },
+  {
+    "revision": "0489801698c582b71fc7",
+    "url": "/weather/static/js/2.1eacf6fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.1eacf6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8105a43483487808da49",
+    "url": "/weather/static/js/main.d8d576c1.chunk.js"
+  },
+  {
+    "revision": "0b1a00671a4dff144826",
+    "url": "/weather/static/js/runtime-main.c344ef46.js"
+  },
+  {
+    "revision": "589f26a101ce82f1a9ad0710af5e0ebf",
+    "url": "/weather/static/media/background.589f26a1.jpg"
+  }
+]);
